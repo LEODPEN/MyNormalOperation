ba958577c0801724130dfca2ea3f23f6b721f7ae
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-02-10 00:26:11 +0800

    thread

--- threads/src/Actor.java
+++ threads/src/Actor.java
@@ -1,15 +1,58 @@
-public class Actor extends Thread{
-    public void run(){
-        System.out.println(getName()+"是一个演员");
-        int count =0;
-        System.out.println(getName()+"登台演出:" + (++count));
-
-
-        System.out.println(getName()+"演出结束!");
-    }
-    public static void main(String[] args){
+public class Actor extends Thread {
+    public static void main(String[] args) {
         Thread actor = new Actor();
         actor.setName("Mr.thread");
         actor.start();
+        Thread actressThread = new Thread(new Actress(),"Ms.Runnable");
+        actressThread.start();
     }
+
+    public void run() {
+        System.out.println(getName() + "是一个演员");
+        int count = 0;
+        boolean keepRunning = true;
+        while (keepRunning) {
+            System.out.println(getName() + "登台演出:" + (++count));
+
+            if (count == 100) {
+                keepRunning = false;
+            }
+
+            if (count % 10 == 0) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println(getName() + "演出结束!");
+    }
+
 }
+
+class Actress implements Runnable{
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+"是一个演员");
+        int count=0;
+        boolean keepRunning=true;
+        while(keepRunning){
+        System.out.println(Thread.currentThread().getName()+"登台演出:"+(++count));
+
+        if(count==100){
+        keepRunning=false;
+        }
+
+        if(count%10==0){
+        try{
+        Thread.sleep(1000);
+        }catch(InterruptedException e){
+        e.printStackTrace();
+        }
+        }
+        }
+
+        System.out.println(Thread.currentThread().getName()+"演出结束!");
+        }
+        }
