66e9469b50a7db6721ce70064a6b3fca8c6e7030
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-02-18 00:41:17 +0800

    spring boot begin

---
+++ gril/src/main/java/girl/girl/HelloController.java
@@ -0,0 +1,29 @@
+package girl.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController//等于 controller和responsebody同时使用的效果
+public class HelloController {
+
+
+    @Autowired
+    private girlProperties Girl;
+
+    //使用方式：/hello/234
+
+    /*@RequestMapping(value = {"/hello/{id}"},method = RequestMethod.GET)
+    public String say(@PathVariable("id") Integer id){
+        return "id: " + id;
+        //return Girl.getCupSize();
+    }*/
+
+    //使用 /hello?id=234
+    @RequestMapping(value = {"/hello"},method = RequestMethod.GET)
+    //上一句可以简化为：@GetMapping(value = "/hello")  简单就好 联想到 @
+
+    public String say(@RequestParam(value = "id",required = false, defaultValue = "0") Integer myId){
+        return "id: " + myId;
+        //return Girl.getCupSize();
+    }
+}
---
+++ gril/src/main/java/girl/girl/girlProperties.java
@@ -0,0 +1,28 @@
+package girl.girl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class girlProperties {
+    private String cupSize;
+    private Integer age;
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
+
+
+}
