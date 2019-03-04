190d8161b2b1bb3300c5125a24084e17078c65c6
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-03-05 00:05:59 +0800

    api测试
    以controller中list为例

--- girl/src/test/java/girl/girl/controller/GirlControllerTest.java
+++ girl/src/test/java/girl/girl/controller/GirlControllerTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +20,12 @@ import static org.junit.Assert.*;
 public class GirlControllerTest {
 
     @Autowired
-    private GirlController girlController;
+    private MockMvc mvc;
 
     @Test
     public void girlList() throws Exception {
-        girlController.girlList();
+        mvc.perform(MockMvcRequestBuilders.get("/girls"))
+                .andExpect(MockMvcResultMatchers.status().isOk());//对返回的状态码进行判断
+                //.andExpect(MockMvcResultMatchers.content().string("期望的字符串（结果）"));
     }
 }
